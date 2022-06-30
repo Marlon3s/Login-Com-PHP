d8adbccbe53a53901a5137105d5d3dd5a50abeb7
Marlom-Raul <79466275+Marlon3s@users.noreply.github.com>
2022-06-30 13:59:08 -0300

    Commenting script

--- LoginComPHP/login.php
+++ LoginComPHP/login.php
@@ -8,12 +8,16 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])){
     exit();
 }
 
+//Variáveis de controle contra SQLInjection
 $user = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $password = mysqli_real_escape_string($conexao, $_POST['senha']);
 
 $query = "select usuario_id, usuario from usuario where usuario = '{$user}' and senha = md5('{$password}')";
+
+//Executando query de SELECT
 $result = mysqli_query($conexao, $query);
 
+//Validando se o usuário existe na base de dados
 $row = mysqli_num_rows($result);
 if($row == 1) {
     $_SESSION['usuario'] = $user;
